3143dea17baacb8db37d18815e9653a1752fba42
Miguel <miguezaga2@hotmail.com>
2022-12-03 10:04:23 -0500

    add html structure

--- src/App.js
+++ src/App.js
@@ -3,49 +3,88 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-  Try It Free
+      <header className="header">
+        <img alt="" src="" />
+        <button className="header__btn">Try It Free</button>
+      </header>
+      <main className="main">
+        <section className="hero">
+          <div className="hero__container">
+            <h1>Build The Community Your Fans Will Love</h1>
+            <p>
+              Huddle re-imagines the way we build communities. You have a voice,
+              but so does your audience. Create connections with your users as
+              you engage in genuine discussion.
+            </p>
 
-  Build The Community Your Fans Will Love
+            <button>Get Started For Free</button>
+          </div>
+          <img alt="" src="" />
+        </section>
+        <section className="features">
+          <article>
+            <img alt="" src="" />
+            <h2>Grow Together</h2>
+            <p>
+              Generate meaningful discussions with your audience and build a
+              strong, loyal community. Think of the insightful conversations you
+              miss out on with a feedback form.
+            </p>
+          </article>
+          <article>
+            <img alt="" src="" />
+            <h2>Flowing Conversations</h2>
+            <p>
+              You wouldn't paginate a conversation in real life, so why do it
+              online? Our threads have just-in-time loading for a more natural
+              flow.
+            </p>
+          </article>
 
-  Huddle re-imagines the way we build communities. You have a voice, but so does your audience. 
-  Create connections with your users as you engage in genuine discussion. 
+          <article>
+            <img alt="" src="" />
+            <h2>Your Users</h2>
+            <p>
+              It takes no time at all to integrate Huddle with your app's
+              authentication solution. This means, once signed in to your app,
+              your users can start chatting immediately.
+            </p>
+          </article>
+        </section>
+        <section className="cta">
+          <h2>Ready To Build Your Community?</h2>
+          <button>Get Started For Free</button>
+        </section>
+      </main>
+      <footer className="footer">
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </li>
+          <li>+1-543-123-4567</li>
+          <li>example@huddle.com</li>
+        </ul>
 
-  Get Started For Free
+        <nav>
+          <ul>
+            <li>About Us</li>
+            <li>What We Do</li>
+            <li>FAQ</li>
 
-  Grow Together
+            <li>Career</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+          </ul>
+        </nav>
 
-  Generate meaningful discussions with your audience and build a strong, loyal community. 
-  Think of the insightful conversations you miss out on with a feedback form. 
-
-  Flowing Conversations
-
-  You wouldn't paginate a conversation in real life, so why do it online? Our threads 
-  have just-in-time loading for a more natural flow.
-
-  Your Users
-
-  It takes no time at all to integrate Huddle with your app's authentication solution. 
-  This means, once signed in to your app, your users can start chatting immediately.
-
-  Ready To Build Your Community?
-
-  Get Started For Free
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-  incididunt ut labore et dolore magna aliqua
-
-  +1-543-123-4567
-  example@huddle.com
-
-  About Us
-  What We Do
-  FAQ
-
-  Career
-  Blog
-  Contact Us
-
-  &copy; Copyright 2018 Huddle. All rights reserved.
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <p>&copy; Copyright 2018 Huddle. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
